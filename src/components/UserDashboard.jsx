9053d71d0ff9e9f9add5133acd521f4d000d2860
Manojkumar0308 <manojku0308@gmail.com>
2024-10-16 18:05:57 +0530

    chart implemented but not aligned

--- src/components/UserDashboard.jsx
+++ src/components/UserDashboard.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronDown, faCloud, faMapMarkerAlt, faSun, faThermometerFull, faThunderstorm, faWind } from '@fortawesome/free-solid-svg-icons';
+import { faChevronDown, faCloud, faGauge, faMapMarkerAlt, faSolarPanel, faSun, faThermometerFull, faThunderstorm, faTriangleExclamation, faWind } from '@fortawesome/free-solid-svg-icons';
 import backgroundImage from "../asset/images/solar-panel-dashboard.jpg";
 import { faThermometerHalf } from "@fortawesome/free-solid-svg-icons/faThermometerHalf";
 import { faWater } from "@fortawesome/free-solid-svg-icons/faWater";
 import MobileNavbar from "./MobileNavBar";
 import { faPowerOff } from "@fortawesome/free-solid-svg-icons/faPowerOff";
+import { faMeteor } from "@fortawesome/free-solid-svg-icons/faMeteor";
+import DashboardGraphs from "./UserDashboardGraph";
 const UserDashBoard = ({ isSidebarOpen, toggleSidebar, setActiveTab }) => {
     return (
         <div className="min-w-full bg-white h-screen flex flex-col">
             <MobileNavbar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} setActiveTab={setActiveTab}></MobileNavbar>
-            <div className="flex flex-col xl:flex-row  gap-2 mt-4 p-8 h-[35vh]">
-                <div className="flex flex-3 flex-col gap-2 ">
-                    <div className="h-[35vh] rounded-lg p-2 bg-cover"  style={{ backgroundImage: `url(${backgroundImage})` }} >
+            <div className="grid grid-cols-1 md:grid-cols-2  xl:grid-cols-3 gap-2 mt-4 p-4">
+                <div className="flex flex-1 flex-col gap-2 h-[32vh] ">
+                    <div className="h-full object-cover  rounded-lg p-2 bg-cover"  style={{ backgroundImage: `url(${backgroundImage})` }} >
                         <div className="flex flex-row gap-2  justify-center">
                             <span>
                                 <FontAwesomeIcon icon={faMapMarkerAlt} className="text-lg text-white" />
@@ -37,7 +39,7 @@ const UserDashBoard = ({ isSidebarOpen, toggleSidebar, setActiveTab }) => {
                     
                 </div>
 
-                <div className="flex flex-1 flex-col gap-2 border border-gray-200 rounded-lg">
+                <div className="flex flex-1 flex-col gap-2 border border-gray-200 rounded-lg ">
                    <div className="flex flex-row justify-between items-center p-4 ">
                     <span className="text-sm font-semibold">Generation<FontAwesomeIcon icon={faChevronDown} className="text-[8px] ml-1 mb-[1.6px]"/></span>
                     <span className="text-xs font-semibold text-gray-500">June 25-12:30 Pm<FontAwesomeIcon icon={faChevronDown} className="text-[6px] ml-1 mb-[1.6px]"/></span>
@@ -66,65 +68,52 @@ const UserDashBoard = ({ isSidebarOpen, toggleSidebar, setActiveTab }) => {
                    </div>
                     
                 </div>
-                 {/*Current Weather*/}
-                <div className="flex flex-1 flex-col gap-2">
-                   <div className="flex border justify-center border-gray-200 rounded-lg py-2">
+                 {/*Sensors reading*/}
+                <div className="flex flex-1 flex-col gap-[14px] md:col-span-2 xl:col-span-1">
+                   <div className="flex border justify-center border-gray-200 rounded-lg py-2 ">
                     <span className="text-sm font-semibold">
                         Current weather
                     </span>
                    </div>
                    
-                    <div className="flex flex-row gap-2">
-                        <div className="flex flex-row gap-4 items-center justify-center border border-gray-200 rounded-lg py-2 px-4 w-1/2">
+                   <div className="grid grid-cols-2 md:grid-cols-4 xl:grid-cols-2 gap-2">
+                 <div className="flex flex-row gap-4 items-center justify-center border border-gray-200 rounded-lg py-2 px-4">
                             <FontAwesomeIcon icon={faSun} className="text-[20px]"/>
                             <div className="flex flex-col">
                                 <span className="text-sm font-semibold">378.79Wh/m2</span>
                                 <span className="text-xs font-semibold text-gray-200">Light intensity</span>
                             </div>
                         </div>
-
-                       
-
-                        
-
-                        <div className="flex flex-row gap-4 items-center justify-center border border-gray-200 rounded-lg py-2 px-4 w-1/2">
+                        <div className="flex flex-row gap-4 items-center justify-center border border-gray-200 rounded-lg py-2 px-4 ">
                             <FontAwesomeIcon icon={faThermometerFull} className="text-[20px]"/>
                             <div className="flex flex-col">
                                 <span className="text-sm font-semibold">21.88*C</span>
                                 <span className="text-xs font-semibold text-gray-200">Temperature</span>
                             </div>
                         </div>
-                    </div>
+                        <div className="flex   flex-row gap-4 items-center justify-center border border-gray-200 rounded-lg py-2 px-4">
+                            <FontAwesomeIcon icon={faSun} className="text-[20px]"/>
+                            <div className="flex flex-col">
+                                <span className="text-sm font-semibold">378.79Wh/m2</span>
+                                <span className="text-xs font-semibold text-gray-200">Light intensity</span>
+                            </div>
+                        </div>
+                        <div className="flex   flex-row gap-4 items-center justify-center border border-gray-200 rounded-lg py-2 px-4 ">
+                            <FontAwesomeIcon icon={faThermometerFull} className="text-[20px]"/>
+                            <div className="flex flex-col">
+                                <span className="text-sm font-semibold">21.88*C</span>
+                                <span className="text-xs font-semibold text-gray-200">Temperature</span>
+                            </div>
+                        </div>
 
-                    <div className="flex flex-row gap-2">
-                        <div className="flex flex-row gap-4 items-center justify-center border border-gray-200 rounded-lg py-2 px-4 w-1/2">
+                        <div className="flex   flex-row gap-4 items-center justify-center border border-gray-200 rounded-lg py-2 px-4">
                             <FontAwesomeIcon icon={faCloud} className="text-[20px]"/>
                             <div className="flex flex-col">
                                 <span className="text-sm font-semibold">71.00%rh</span>
                                 <span className="text-xs font-semibold text-gray-200">Humidity</span>
                             </div>
                         </div>
-
-                        <div className="flex flex-row gap-4 items-center justify-center border border-gray-200 rounded-lg py-2 px-4 w-1/2">
-                            <FontAwesomeIcon icon={faThermometerHalf} className="text-[20px]"/>
-                            <div className="flex flex-col ">
-                                <span className="text-sm font-semibold">40.53*C</span>
-                                <span className="text-xs font-semibold text-gray-200">Module Temperature</span>
-                                {/* <span className="text-md font-semibold text-gray-200">Temperature</span> */}
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="flex flex-row gap-2">
-                        <div className="flex flex-row gap-4 items-center justify-center border border-gray-200 rounded-lg py-2 px-4 w-1/2">
-                            <FontAwesomeIcon icon={faWind} className="text-[20px]"/>
-                            <div className="flex flex-col">
-                                <span className="text-sm font-semibold">1013.25mb</span>
-                                <span className="text-xs font-semibold text-gray-200">Pressure</span>
-                            </div>
-                        </div>
-
-                        <div className="flex flex-row gap-4 items-center justify-center border border-gray-200 rounded-lg py-2 px-4 w-1/2">
+                        <div className="flex   flex-row gap-4 items-center justify-center border border-gray-200 rounded-lg py-2 px-4 ">
                             <FontAwesomeIcon icon={faWater}  className="text-[20px]"/>
                             <div className="flex flex-col ">
                                 <span className="text-sm font-semibold">2.6 m/s</span>
@@ -132,10 +121,8 @@ const UserDashBoard = ({ isSidebarOpen, toggleSidebar, setActiveTab }) => {
                                 <span className="text-xs font-semibold text-gray-200">Wind</span>
                             </div>
                         </div>
-                    </div>
-                    {/*current and Voltage */}
-                    <div className="flex flex-row gap-2">
-                        <div className="flex flex-row gap-4 items-center justify-center border border-gray-200 rounded-lg py-2 px-4 w-1/2">
+
+                        <div className="flex  flex-row gap-4 items-center justify-center border border-gray-200 rounded-lg py-2 px-4 ">
                             <FontAwesomeIcon icon={faThunderstorm} className="text-[20px]"/>
                             <div className="flex flex-col">
                                 <span className="text-sm font-semibold">1013.25mb</span>
@@ -143,7 +130,7 @@ const UserDashBoard = ({ isSidebarOpen, toggleSidebar, setActiveTab }) => {
                             </div>
                         </div>
 
-                        <div className="flex flex-row gap-4 items-center justify-center border border-gray-200 rounded-lg py-2 px-4 w-1/2">
+                        <div className="flex   flex-row gap-4 items-center justify-center border border-gray-200 rounded-lg py-2 px-4 ">
                             <FontAwesomeIcon icon={faPowerOff}  className="text-[20px]"/>
                             <div className="flex flex-col ">
                                 <span className="text-sm font-semibold">2.6 m/s</span>
@@ -151,11 +138,41 @@ const UserDashBoard = ({ isSidebarOpen, toggleSidebar, setActiveTab }) => {
                                 <span className="text-xs font-semibold text-gray-200">Voltage</span>
                             </div>
                         </div>
-                    </div>
-                  
-                    
+                 </div>            
                 </div>
             </div>
+
+            {/* Efficiency and yield section */}
+            <div className="border border-gray-200 rounded-lg mx-4">
+    <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-2 p-2 ">
+        <div className="flex flex-col justify-center items-center gap-2 bg-yellow-100 rounded-lg py-2 min-w-[100px]">
+            <FontAwesomeIcon icon={faGauge} className="text-[25px]" />
+            <span className="text-[14px] font-semibold text-gray-500">Performance ratio</span>
+            <span className="text-[14px] font-semibold">69.1326%</span>
+        </div>
+
+        <div className="flex flex-col justify-center items-center gap-2 bg-purple-100 rounded-lg py-2 min-w-[100px]">
+            <FontAwesomeIcon icon={faSun} className="text-[25px]" />
+            <span className="text-[14px] font-semibold text-gray-500">CUF</span>
+            <span className="text-[14px] font-semibold">69.1326%</span>
+        </div>
+
+        <div className="flex flex-col justify-center items-center gap-2 bg-blue-100 rounded-lg py-2 min-w-[100px]">
+            <FontAwesomeIcon icon={faTriangleExclamation} className="text-[25px]" />
+            <span className="text-[14px] font-semibold text-gray-500">Specific yield</span>
+            <span className="text-[14px] font-semibold">69.1326%</span>
+        </div>
+
+        <div className="flex flex-col justify-center items-center gap-2 bg-green-100 rounded-lg py-2 min-w-[100px]">
+            <FontAwesomeIcon icon={faSolarPanel} className="text-[25px]" />
+            <span className="text-[14px] font-semibold text-gray-500">Insolation today</span>
+            <span className="text-[14px] font-semibold">69.1326%</span>
+        </div>
+    </div>
+   
+</div>
+<DashboardGraphs />
+
         </div>
     );
 };
