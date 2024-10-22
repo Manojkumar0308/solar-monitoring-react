6c772ba9a8237d975b4ef2390216143562a5945a
Manojkumar0308 <manojku0308@gmail.com>
2024-10-22 16:16:46 +0530

    Table styling done but column names are not getting Bold

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,17 +3,34 @@ import Box from '@mui/material/Box';
 import { DataGrid } from '@mui/x-data-grid';
 import MobNavBar from './MobileNavBar';
 import { Typography } from '@mui/material';
+import '@mui/material';
+import CheckIcon from '@mui/icons-material/Check';  
+import ClearIcon from '@mui/icons-material/Clear';
 
-
+const formatDate = (date) => {
+  return date.toLocaleString('en-US', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+  });
+};
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 90,headerClassName: 'table-header', headerAlign: 'center', },
+  { field: 'id', headerName: 'ID', width: 90, headerAlign: 'center',  renderCell: (params) => <div className='flex justify-center items-center'>{params.value
+  }</div>,
+ },
   {
     field: 'siteId',
     headerName: 'Site-Id',
     width: 150,
     editable: false,
     headerAlign: 'center',
+    renderCell: (params) => <div className='flex justify-center items-center'>{params.value
+    }</div>,
+   
     
   },
   {
@@ -22,6 +39,10 @@ const columns = [
     width: 150,
     editable: false,
     headerAlign: 'center',
+    flex :1,
+    renderCell: (params) => <div className='flex justify-center items-center'>{params.value
+    }</div>,
+   
   },
   {
     field: 'email',
@@ -29,6 +50,9 @@ const columns = [
     width: 150,
     editable: false,
     headerAlign: 'center',
+    renderCell: (params) => <div className='flex justify-center items-center'>{params.value
+    }</div>,
+   
   },
   {
     field: 'contact',
@@ -36,13 +60,19 @@ const columns = [
     width: 150,
     editable: false,
     headerAlign: 'center',
+    renderCell: (params) => <div className='flex justify-center items-center'>{params.value
+    }</div>,
+   
   },
   {
     field: 'address',
     headerName: 'Address',
     width: 150,
     editable: false,
+    flex:2,
     headerAlign: 'center',
+    renderCell: (params) => <div className='flex justify-center items-center' >{params.value}</div>,
+   
   },
   {
     field: 'city',
@@ -50,6 +80,9 @@ const columns = [
     width: 110,
     editable: false,
     headerAlign: 'center',
+    renderCell: (params) => <div className='flex justify-center items-center'>{params.value
+    }</div>,
+   
   },
   {
     field: 'status',
@@ -57,6 +90,14 @@ const columns = [
     width: 110,
     editable: false,
     headerAlign: 'center',
+    renderCell: (params) => <div className='flex items-center justify-center h-full'>
+    {params.value === 'Active' ? (
+      <CheckIcon className='text-green-500' />
+    ) : (
+      <ClearIcon className='text-red-500' />
+    )}
+  </div>
+   
   },
   {
     field: 'createdAt',
@@ -65,21 +106,29 @@ const columns = [
     width: 180,
     editable: false,
     headerAlign: 'center',
+    
+    renderCell: (params) => (
+      <div className='flex justify-center items-center'>
+        {formatDate(new Date(params.value))} {/* Format the date here */}
+      </div>
+    ),
+   
   },
+  
 ];
 
 const rows = [
   { id: 1, siteId: 'S123', siteName: 'MJSH Plant', email: 'site1@example.com', contact: '1234567890', address: 'Plot-12334, abc nagar, abc Road', city: 'Noida', status: 'Active', createdAt: new Date() },
-  { id: 2, siteId: 'S124', siteName: 'MJSH Plant', email: 'site2@example.com', contact: '9876543210', address: 'Plot-12335, abc nagar, abc Road', city: 'Noida', status: 'Active', createdAt: new Date() },
+  { id: 2, siteId: 'S124', siteName: 'MJSH Plant', email: 'site2@example.com', contact: '9876543210', address: 'Plot-12335, abc nagar, abc Road', city: 'Noida', status: 'InActive', createdAt: new Date() },
   { id: 3, siteId: 'S125', siteName: 'MJSH Plant', email: 'site3@example.com', contact: '6543219870', address: 'Plot-12336, abc nagar, abc Road', city: 'Noida', status: 'Active', createdAt: new Date() },
   { id: 4, siteId: 'S126', siteName: 'MJSH Plant', email: 'site4@example.com', contact: '3216549870', address: 'Plot-12337, abc nagar, abc Road', city: 'Noida', status: 'Active', createdAt: new Date() },
   { id: 5, siteId: 'S127', siteName: 'MJSH Plant', email: 'site5@example.com', contact: '9871236540', address: 'Plot-12338, abc nagar, abc Road', city: 'Noida', status: 'Active', createdAt: new Date() },
   { id: 6, siteId: 'S128', siteName: 'MJSH Plant', email: 'site6@example.com', contact: '1472583690', address: 'Plot-12339, abc nagar, abc Road', city: 'Noida', status: 'Active', createdAt: new Date() },
-  { id: 7, siteId: 'S129', siteName: 'MJSH Plant', email: 'site7@example.com', contact: '2583691470', address: 'Plot-12340, abc nagar, abc Road', city: 'Noida', status: 'Active', createdAt: new Date() },
+  { id: 7, siteId: 'S129', siteName: 'MJSH Plant', email: 'site7@example.com', contact: '2583691470', address: 'Plot-12340, abc nagar, abc Road', city: 'Noida', status: 'InActive', createdAt: new Date() },
   { id: 8, siteId: 'S130', siteName: 'MJSH Plant', email: 'site8@example.com', contact: '3691472580', address: 'Plot-12341, abc nagar, abc Road', city: 'Noida', status: 'Active', createdAt: new Date() },
   { id: 9, siteId: 'S131', siteName: 'MJSH Plant', email: 'site9@example.com', contact: '1473692580', address: 'Plot-12342, abc nagar, abc Road', city: 'Noida', status: 'Active', createdAt: new Date() },
   { id: 10, siteId: 'S132', siteName: 'MJSH Plant', email: 'site10@example.com', contact: '2581473690', address: 'Plot-12343, abc nagar, abc Road', city: 'Noida', status: 'Active', createdAt: new Date() },
-  { id: 11, siteId: 'S133', siteName: 'MJSH Plant', email: 'site11@example.com', contact: '9638527410', address: 'Plot-12344, abc nagar, abc Road', city: 'Noida', status: 'Active', createdAt: new Date() },
+  { id: 11, siteId: 'S133', siteName: 'MJSH Plant', email: 'site11@example.com', contact: '9638527410', address: 'Plot-12344, abc nagar, abc Road', city: 'Noida', status: 'InActive', createdAt: new Date() },
   { id: 12, siteId: 'S134', siteName: 'MJSH Plant', email: 'site12@example.com', contact: '8527419630', address: 'Plot-12345, abc nagar, abc Road', city: 'Noida', status: 'Active', createdAt: new Date() },
   { id: 13, siteId: 'S135', siteName: 'MJSH Plant', email: 'site13@example.com', contact: '7418529630', address: 'Plot-12346, abc nagar, abc Road', city: 'Noida', status: 'Active', createdAt: new Date() },
   { id: 14, siteId: 'S136', siteName: 'MJSH Plant', email: 'site14@example.com', contact: '3692581470', address: 'Plot-12347, abc nagar, abc Road', city: 'Noida', status: 'Active', createdAt: new Date() },
@@ -100,7 +149,7 @@ export default function DataGridDemo({ isSidebarOpen, toggleSidebar, setActiveTa
       )}
       {showMobNavBar && (<h1 className='text-3xl font-bold p-8'>Site List</h1>)}
 
-      <Box sx={{ width: '100%' }} className="mt-8 px-8">
+      <Box sx={{ width: '100%',textAlign: 'center' }} className="mt-8 px-8">
        
         <DataGrid
           rows={rows}
